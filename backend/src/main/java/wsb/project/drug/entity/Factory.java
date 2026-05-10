2e8abab395744fd110665f1471db0bf0eca0c0c8
Mihu7 <163681854+Mihu7776@users.noreply.github.com>
2026-05-10 13:51:23 +0200

    .

---
+++ backend/src/main/java/wsb/project/drug/entity/Factory.java
@@ -0,0 +1,15 @@
+package wsb.project.drug.entity;
+
+import jakarta.persistence.GeneratedValue;
+
+@entity
+@Table(name"dfhhjfd")
+
+public class Factory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name'sdgsdgsd',lenght = 145)
+    private String name;
+
+}
